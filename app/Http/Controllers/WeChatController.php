a27e4d40fc24afa172f1d245c556bdb36cd63156
kaka <1519176958@qq.com>
2019-01-15 16:15:33 +0800

    回复图片

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use EasyWeChat\Kernel\Messages\Image;
 use Illuminate\Support\Facades\DB;
 use Log;
 
@@ -13,11 +14,46 @@ class WeChatController extends Controller
 
     $app = app('wechat.official_account');
     $app->server->push(function ($message) {
-      $count = DB::table('qiu_shi_bai_kes')->count();
 
-      $data = DB::table('qiu_shi_bai_kes')->find(rand(1, $count));
-      $content = $data->content;
-      return str_replace("<br>", "\n", $content);
+
+      switch ($message['MsgType']) {
+        case 'event':
+          return '收到事件消息';
+          break;
+        case 'text':
+          if ($message['Content'] == '图片') {
+            $count = DB::table('img_lists')->count();
+            $data = DB::table('img_lists')->find(rand(1, $count));
+            $mediaId = $data->midea_id;
+            return new Image($mediaId);
+          }
+
+          $count = DB::table('qiu_shi_bai_kes')->count();
+          $data = DB::table('qiu_shi_bai_kes')->find(rand(1, $count));
+          $content = $data->content;
+          return str_replace("<br>", "\n", $content);
+          break;
+        case 'image':
+          return '收到图片消息';
+          break;
+        case 'voice':
+          return '收到语音消息';
+          break;
+        case 'video':
+          return '收到视频消息';
+          break;
+        case 'location':
+          return '收到坐标消息';
+          break;
+        case 'link':
+          return '收到链接消息';
+          break;
+        case 'file':
+          return '收到文件消息';
+        default:
+          return '收到其它消息';
+          break;
+      }
     });
 
     $response = $app->server->serve();
