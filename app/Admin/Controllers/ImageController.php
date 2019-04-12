b06078685313de1e1b2b750d7e4b8884115f1567
kakashen <1519176958@qq.com>
2019-04-12 14:48:49 +0800

    图片名称处理

--- app/Admin/Controllers/ImageController.php
+++ app/Admin/Controllers/ImageController.php
@@ -112,7 +112,7 @@ class ImageController extends Controller
 
         $form->select('image_classification_id', '图片分类')->options('/api/image_classifications');
         $form->image('picture')->name(function ($file) {
-            dd($file);
+            return $file->originalName;
         });
 
 
