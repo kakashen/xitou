1b531a46e75a48d9e5a530a4be6cb08301db72b0
kakashen <1519176958@qq.com>
2019-04-12 09:15:18 +0800

    sport路由

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -12,4 +12,6 @@ Route::group([
 
     $router->get('/', 'HomeController@index');
     $router->resource('sporter', SporterController::class);
+    $router->resource('sport', SportController::class);
+
 });
