945f741f3c7964fd87dbb0dacb592de24d696e1c
kakashen <1519176958@qq.com>
2019-04-12 09:13:00 +0800

    Sport 控制器

---
+++ app/Admin/Controllers/SportController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Sport;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\HasResourceActions;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Show;
+
+class SportController extends Controller
+{
+    use HasResourceActions;
+
+    /**
+     * Index interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function index(Content $content)
+    {
+        return $content
+            ->header('Index')
+            ->description('description')
+            ->body($this->grid());
+    }
+
+    /**
+     * Show interface.
+     *
+     * @param mixed $id
+     * @param Content $content
+     * @return Content
+     */
+    public function show($id, Content $content)
+    {
+        return $content
+            ->header('Detail')
+            ->description('description')
+            ->body($this->detail($id));
+    }
+
+    /**
+     * Edit interface.
+     *
+     * @param mixed $id
+     * @param Content $content
+     * @return Content
+     */
+    public function edit($id, Content $content)
+    {
+        return $content
+            ->header('Edit')
+            ->description('description')
+            ->body($this->form()->edit($id));
+    }
+
+    /**
+     * Create interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function create(Content $content)
+    {
+        return $content
+            ->header('Create')
+            ->description('description')
+            ->body($this->form());
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Sport);
+
+        $grid->id('Id');
+        $grid->sporter_id('Sporter id');
+        $grid->date('Date');
+        $grid->recommend('Recommend');
+        $grid->result('Result');
+        $grid->created_at('Created at');
+        $grid->updated_at('Updated at');
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Sport::findOrFail($id));
+
+        $show->id('Id');
+        $show->sporter_id('Sporter id');
+        $show->date('Date');
+        $show->recommend('Recommend');
+        $show->result('Result');
+        $show->created_at('Created at');
+        $show->updated_at('Updated at');
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Sport);
+
+        $form->switch('sporter_id', 'Sporter id');
+        $form->date('date', 'Date')->default(date('Y-m-d'));
+        $form->text('recommend', 'Recommend');
+        $form->switch('result', 'Result')->default(2);
+
+        return $form;
+    }
+}
