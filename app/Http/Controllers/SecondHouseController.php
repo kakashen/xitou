bf7c028b29fdd54f8b008a501349fa59be287443
kakashen <1519176958@qq.com>
2019-04-25 15:51:16 +0800

    添加api 域名

---
+++ app/Http/Controllers/SecondHouseController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\SecondHouse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class SecondHouseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\SecondHouse  $secondHouse
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SecondHouse $secondHouse)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\SecondHouse  $secondHouse
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SecondHouse $secondHouse)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\SecondHouse  $secondHouse
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SecondHouse $secondHouse)
+    {
+        $link = $request->get('link');
+        $all = $request->all();
+        unset($all['link']);
+
+        return  new Response($secondHouse->update(['link' => $link], $all));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\SecondHouse  $secondHouse
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SecondHouse $secondHouse)
+    {
+        //
+    }
+}
